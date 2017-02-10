b574e481c23cac8ccb4e1c8faffbf1ac3ee6fdf1
James Sear <james.s@mmtdigital.co.uk>
2017-02-10 15:58:42 +0000

    Tabs —> spaces

--- webpack.config.js
+++ webpack.config.js
@@ -7,28 +7,28 @@ module.exports = {
     filename: 'bundle.js',
     path: 'src/_static'
   },
-	watch: true,
-	module: {
-		loaders: [
-			{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-			{ test: /\.json$/, loader: 'json-loader' },
-		]
-	},
-	performance: {
-  	hints: false
-	},
-	// Webpack dev server is not currently used...
-	devServer: {
-		contentBase: path.join(__dirname, "/"),
-		stats: {
-			colors: true,
-			hash: false,
-			version: false,
-			timings: false,
-			assets: false,
-			chunks: false,
-			warnings: false
-		},
-		clientLogLevel: "error"
-	}
+  watch: true,
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.json$/, loader: 'json-loader' },
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  // Webpack dev server is not currently used...
+  devServer: {
+    contentBase: path.join(__dirname, "/"),
+    stats: {
+      colors: true,
+      hash: false,
+      version: false,
+      timings: false,
+      assets: false,
+      chunks: false,
+      warnings: false
+    },
+    clientLogLevel: "error"
+  }
 }
